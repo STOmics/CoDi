76987b7a9197ea351036c5cc62d227d32c4f7d62
Nikola Milicevic <nikola260896@gmail.com>
2023-10-03 13:23:26 +0000

    Added CLI arguments

--- ssi.py
+++ ssi.py
@@ -1,20 +1,62 @@
-import scanpy as sc
-import pandas as pd
-from scipy.spatial.distance import mahalanobis
-import numpy as np
-import seaborn as sns
+import argparse as ap
+from collections import Counter
+import logging
+import random
+
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
-import random
-from collections import Counter
+import numpy as np
+import pandas as pd
+import scanpy as sc
+from scipy.spatial.distance import mahalanobis
+import seaborn as sns
 
 
-st_path = '/home/vlada/ssi/data/4K/Mouse_brain_ST.h5ad'  # /goofys/projects/SSI/datasets
-sc_path = '/home/vlada/ssi/data/4K/Mouse_brain_SC.h5ad'
-adata_sc = sc.read_h5ad(sc_path)
-adata_st = sc.read_h5ad(st_path)
-sc_df_raw = pd.DataFrame(adata_sc.X, index=adata_sc.obs.index, columns=adata_sc.var.index).copy()
-st_df_raw = pd.DataFrame(adata_st.X.toarray(), index=adata_st.obs.index, columns=adata_st.var.index).copy()
+logging.basicConfig(
+    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", level=logging.DEBUG
+)
+logger = logging.getLogger(__name__)
+
+parser = ap.ArgumentParser(description="A script that performs SSI.")
+parser.add_argument(
+    "--sc_path", help="A single cell reference dataset", type=str, required=True
+)
+parser.add_argument(
+    "--st_path", help="A spatially resolved dataset", type=str, required=True
+)
+parser.add_argument(
+    "-a",
+    "--annotation",
+    help="Annotation label for cell types",
+    type=str,
+    required=False,
+    default="cell_subclass",
+)
+parser.add_argument(
+    "--num_markers",
+    help="Number of marker genes",
+    type=int,
+    required=False,
+    default=100,
+)
+parser.add_argument(
+    "--num_threads",
+    help="Number of threads that will be used to speed up community calling",
+    type=int,
+    required=False,
+    default=5,
+)
+
+args = parser.parse_args()
+
+adata_sc = sc.read_h5ad(args.sc_path)
+adata_st = sc.read_h5ad(args.st_path)
+sc_df_raw = pd.DataFrame(
+    adata_sc.X, index=adata_sc.obs.index, columns=adata_sc.var.index
+).copy()
+st_df_raw = pd.DataFrame(
+    adata_st.X.toarray(), index=adata_st.obs.index, columns=adata_st.var.index
+).copy()
 
 # Calculate marker genes
 sc.pp.normalize_total(adata_sc, target_sum=1e4)
@@ -22,10 +64,11 @@ sc.pp.log1p(adata_sc)
 adata_sc.var_names_make_unique()
 sc.pp.highly_variable_genes(adata_sc, inplace=True, n_top_genes=200)
 
-sc.tl.rank_genes_groups(adata_sc, groupby="cell_subclass", use_raw=False)
-markers_df = pd.DataFrame(adata_sc.uns["rank_genes_groups"]["names"]).iloc[0:100, :]
+sc.tl.rank_genes_groups(adata_sc, groupby=args.annotation, use_raw=False)
+markers_df = pd.DataFrame(adata_sc.uns["rank_genes_groups"]["names"]).iloc[
+    0 : args.num_markers, :
+]
 markers = list(np.unique(markers_df.melt().value.values))
-len(markers)
 
 # visualize results
 # sc.pl.rank_genes_groups(adata_sc, key='rank_genes_groups_filtered')
@@ -33,11 +76,14 @@ len(markers)
 # sc.pl.rank_genes_groups_dotplot(adata_sc, key='rank_genes_groups_filtered')
 # hvgs = list(adata_sc.var[adata_sc.var.highly_variable].index)
 markers_intersect = list(set(markers).intersection(adata_st.var.index))
-print(f'Using {len(markers_intersect)} single cell marker genes that exist in ST dataset')
+logger.info(
+    f"Using {len(markers_intersect)} single cell marker genes that exist in ST dataset"
+)
 
 sc_df = sc_df_raw.loc[:, markers_intersect]
 st_df = st_df_raw.loc[:, markers_intersect]
-cell_types = set(adata_sc.obs.cell_subclass)
+cell_types = set(adata_sc.obs[args.annotation])
+
 
 # Algo
 def create_subsets(gene_set, num_of_subsets=10):
@@ -47,6 +93,8 @@ def create_subsets(gene_set, num_of_subsets=10):
         subset_size = random.randint(int(num_of_elem * 0.3), int(num_of_elem * 0.8))
         subsets.append(random.sample(gene_set, subset_size))
     return subsets
+
+
 # *****************************************
 # Precalculate inverse covariance matrices
 # *****************************************
@@ -61,11 +109,15 @@ for ty in cell_types:
     sc_icms[ty] = []
     sc_mean[ty] = []
     for sub_id, subset in enumerate(subsets):
-        subset_df = sc_df[adata_sc.obs['cell_subclass'] == ty][subset]
+        subset_df = sc_df[adata_sc.obs["cell_subclass"] == ty][subset]
         sc_dfs[ty].append(subset_df)
         cm = np.cov(subset_df.values, rowvar=False)  # Calculate covariance matrix
-        sc_icms[ty].append(np.linalg.pinv(cm))  # Use pseudo inverse to avoid error singular matrix (determinant = 0) 
-        sc_mean[ty].append(subset_df.mean())    # ...because of presence of genes with all zero values per cell type
+        sc_icms[ty].append(
+            np.linalg.pinv(cm)
+        )  # Use pseudo inverse to avoid error singular matrix (determinant = 0)
+        sc_mean[ty].append(
+            subset_df.mean()
+        )  # ...because of presence of genes with all zero values per cell type
 
 # *****************************************
 # For all ST cells, for all subsets, for all cell types
@@ -73,34 +125,37 @@ for ty in cell_types:
 for ii in range(len(st_df)):
     best_matches_subsets = []
     for sub_id, subset in enumerate(subsets):
-        best_match = {'cell_type':'', 'dist':9999999}
+        best_match = {"cell_type": "", "dist": 9999999}
         for ty in cell_types:
-            maha = mahalanobis(st_df.iloc[ii, :][subset].values, sc_mean[ty][sub_id].values, sc_icms[ty][sub_id])
+            maha = mahalanobis(
+                st_df.iloc[ii, :][subset].values,
+                sc_mean[ty][sub_id].values,
+                sc_icms[ty][sub_id],
+            )
             # print(ty, maha)
-            if maha < best_match['dist']:
-                best_match = {'cell_type': ty, 'dist': maha}
+            if maha < best_match["dist"]:
+                best_match = {"cell_type": ty, "dist": maha}
         best_matches_subsets.append(best_match)
         # print('-----')
     # print(best_matches_subsets)
- 
+
     # Majority voting
-    cn = Counter([ x['cell_type'] for x in best_matches_subsets])
-    best_match_subset = {'cell_type': cn.most_common(1)[0][0], 'confidence': np.round(cn.most_common(1)[0][1] / num_of_subsets, 3)}
+    cn = Counter([x["cell_type"] for x in best_matches_subsets])
+    best_match_subset = {
+        "cell_type": cn.most_common(1)[0][0],
+        "confidence": np.round(cn.most_common(1)[0][1] / num_of_subsets, 3),
+    }
     assigned_types.append(best_match_subset)
     if ii % 500 == 0:
-        print(f'Processed {ii} out of {len(st_df)}.')
-adata_st.obs['sc_type'] = [ x['cell_type'] for x in assigned_types]
-sns.histplot([ x['confidence'] for x in assigned_types])
-plt.savefig('ssi_confidence_hist.png', dpi=120, bbox_inches='tight')
+        print(f"Processed {ii} out of {len(st_df)}.")
+adata_st.obs["sc_type"] = [x["cell_type"] for x in assigned_types]
+sns.histplot([x["confidence"] for x in assigned_types])
+plt.savefig("ssi_confidence_hist.png", dpi=120, bbox_inches="tight")
+
 
 # Visualisation
 def plot_spatial(
-        adata,
-        annotation,
-        ax: Axes,
-        spot_size: float,
-        palette=None,
-        title: str = ""
+    adata, annotation, ax: Axes, spot_size: float, palette=None, title: str = ""
 ):
     """
     Scatter plot in spatial coordinates.
@@ -118,19 +173,36 @@ def plot_spatial(
     s = spot_size * 0.5
     data = adata
     ax = sns.scatterplot(
-        data=data.obs, hue=annotation, x=data.obsm['spatial'][:, 0], y=data.obsm['spatial'][:, 1],
-        ax=ax, s=s, linewidth=0, palette=palette, marker='.'
+        data=data.obs,
+        hue=annotation,
+        x=data.obsm["spatial"][:, 0],
+        y=data.obsm["spatial"][:, 1],
+        ax=ax,
+        s=s,
+        linewidth=0,
+        palette=palette,
+        marker=".",
     )
     ax.invert_yaxis()
     ax.set(yticklabels=[], xticklabels=[], title=title)
     ax.tick_params(bottom=False, left=False)
     ax.set_aspect("equal")
     sns.despine(bottom=True, left=True, ax=ax)
-             
-palette = {'DGGRC2': '#ffffcc', 'ACNT1':'r', 'TEGLU10':'#a1dab4', 'TEGLU17':'#41b6c4'}
+
+
+palette = {
+    "DGGRC2": "#ffffcc",
+    "ACNT1": "r",
+    "TEGLU10": "#a1dab4",
+    "TEGLU17": "#41b6c4",
+}
 fig, axs = plt.subplots(2, 2, figsize=(14, 14))
-sns.histplot(adata_st.obs['sc_type'], ax=axs[0][0])
-sns.histplot(adata_sc.obs['cell_subclass'], ax=axs[0][1])
-plot_spatial(adata_st, annotation='sc_type', spot_size=30, palette=palette, ax=axs[1][0])
-plot_spatial(adata_st, annotation='celltype', spot_size=30, palette=palette, ax=axs[1][1])
-plt.savefig('ssi.png', dpi=120, bbox_inches='tight')
+sns.histplot(adata_st.obs["sc_type"], ax=axs[0][0])
+sns.histplot(adata_sc.obs["cell_subclass"], ax=axs[0][1])
+plot_spatial(
+    adata_st, annotation="sc_type", spot_size=30, palette=palette, ax=axs[1][0]
+)
+plot_spatial(
+    adata_st, annotation="celltype", spot_size=30, palette=palette, ax=axs[1][1]
+)
+plt.savefig("ssi.png", dpi=120, bbox_inches="tight")
