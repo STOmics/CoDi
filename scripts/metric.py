d28f1d1a765c7b2490b63e2e7fde4b1e5a56d874
Vladimir Kovacevic <upravljac@gmail.com>
2023-10-23 12:20:22 +0000

    Add calculation of top 100 genes.

--- scripts/metric.py
+++ scripts/metric.py
@@ -103,6 +103,20 @@ else:
     markers_per_type_reduced_dict = adata_sc.uns["markers_per_type_reduced_dict"]
     logger.info("Found markers_per_type_reduced_dict in adata_sc.uns")
 
+# Extract top 100 genes
+markers_per_type_top = {}
+markers_df = pd.DataFrame(adata_sc.uns["rank_genes_groups"]["names"])
+pval_df = pd.DataFrame(adata_sc.uns["rank_genes_groups"]["pvals_adj"])
+for col in markers_df.columns:
+    markers_per_type_top[col] = markers_df.loc[pval_df[col] < 0.05, col].values
+# After this for loop all marker genes will be sorted by p-value per cell type
+for col, genes in markers_per_type_top.items():
+    df = pd.DataFrame(markers_df[markers_df[col].isin(genes)][col])
+    df.loc[:, 'pval'] = pval_df.loc[df.index, col]
+    df.sort_values(by='pval', inplace=True)  # Sort by p-val to obtain ranks
+    markers_per_type_top[col] = list(df[col])[:100]
+
+
 # Read ST cell type annotations from CSV
 st_cell_types_df = pd.read_csv(st_cell_type_path)
 st_cell_types_df.set_index(st_cell_types_df.columns[-2], inplace=True)
@@ -118,6 +132,11 @@ adata_st.obs = pd.merge(adata_st.obs, st_cell_types_df, left_index=True, right_i
 
 adata_st = adata_st[adata_st.obs[st_annotation] != "FILTERED"]
 
+st_marker_time = time.time()
+marker_time = np.round(st_marker_time - start_time, 3)
+logger.info(
+    f"Calculate ST marker genes - execution took {marker_time} so far"
+)
 # Calculate ST marker genes
 if "rank_genes_groups" not in adata_st.uns:
     logger.info("Calculating ST marker genes.")
@@ -131,48 +150,61 @@ if "rank_genes_groups" not in adata_st.uns:
     markers_st = list(np.unique(markers_st_df.melt().value.values))
     pval_st_df = pd.DataFrame(adata_st.uns["rank_genes_groups"]["pvals_adj"])
 
-# Remove genes that does not exist in ST
-for ctype, genes in markers_per_type_reduced_dict.items():
-    st_genes = list(adata_st.var.index)
-    markers_per_type_reduced_dict[ctype] = \
-    list(filter(lambda x: x in st_genes, markers_per_type_reduced_dict[ctype]))
+st_marker_time2 = time.time()
+marker_time = np.round(st_marker_time2 - st_marker_time, 3)
+logger.info(
+    f"Calculate of ST marker genes took {marker_time} so far"
+)
 
-markers_per_type_st_dict = dict()
-for col in markers_st_df.columns:
-    df = pd.DataFrame(markers_st_df[col])
-    df.loc[:, 'pval'] = pval_st_df.loc[df.index, col]
-    df.sort_values(by='pval', inplace=True)  # Sort by p-val to obtain ranks
-    df = df[df['pval'] <= 0.05]  # Keep only significant marker genes
-    df.reset_index(inplace=True, drop=True)
-    sc_st_df = pd.DataFrame(markers_per_type_reduced_dict[col], columns=['Gene'])
-    sc_st_df = sc_st_df.merge(df[col].reset_index(), left_on='Gene', right_on=col)[['Gene', 'index']]
-    sc_st_df.columns = ['Gene', 'Rank']
-    markers_per_type_st_dict[col] = list(sc_st_df['Gene'])
-    # display(sc_st_df)
-    # sns.histplot(sc_st_df['Rank'])
-    # plt.show()
+report_df = pd.DataFrame(columns=["sc_marker_genes", "st_marker_genes", "st_cell_types"])
+for sc_dict_name, sc_dict in zip(["unique marker genes", "top 100 marker genes"],
+                                 [markers_per_type_reduced_dict, markers_per_type_top]):
 
-lost_genes = 0
-total_marker_genes_sc = 0
-for ctype, genes in markers_per_type_st_dict.items():  # Can be merged with previous for loop but it is more clear
-    marker_genes_sc = len(markers_per_type_reduced_dict[ctype])
-    dif = marker_genes_sc - len(genes)
-    lost_genes += dif
-    total_marker_genes_sc += marker_genes_sc
-    print(ctype, dif)
+    # Remove genes that does not exist in ST
+    for ctype, genes in sc_dict.items():
+        st_genes = list(adata_st.var.index)
+        sc_dict[ctype] = \
+        list(filter(lambda x: x in st_genes, sc_dict[ctype]))
 
-logger.info(f'Total marker genes in scRNA that also exist in ST (for {len(markers_st_df.columns)} cell types): {total_marker_genes_sc}')
-logger.info(f'Remained marker genes in ST: {total_marker_genes_sc - lost_genes}')
+    markers_per_type_st_dict = dict()
+    for col in markers_st_df.columns:
+        df = pd.DataFrame(markers_st_df[col])
+        df.loc[:, 'pval'] = pval_st_df.loc[df.index, col]
+        df.sort_values(by='pval', inplace=True)  # Sort by p-val to obtain ranks
+        df = df[df['pval'] <= 0.05]  # Keep only significant marker genes
+        df.reset_index(inplace=True, drop=True)
+        sc_st_df = pd.DataFrame(sc_dict[col], columns=['Gene'])
+        sc_st_df = sc_st_df.merge(df[col].reset_index(), left_on='Gene', right_on=col)[['Gene', 'index']]
+        sc_st_df.columns = ['Gene', 'Rank']
+        markers_per_type_st_dict[col] = list(sc_st_df['Gene'])
+        # display(sc_st_df)
+        # sns.histplot(sc_st_df['Rank'])
+        # plt.show()
+
+    lost_genes = 0
+    total_marker_genes_sc = 0
+    for ctype, genes in markers_per_type_st_dict.items():  # Can be merged with previous for loop but it is more clear
+        marker_genes_sc = len(sc_dict[ctype])
+        dif = marker_genes_sc - len(genes)
+        lost_genes += dif
+        total_marker_genes_sc += marker_genes_sc
+        # print(ctype, dif)
+
+    logger.info(f'Total scRNA {sc_dict_name} that also exist in ST (for {len(markers_st_df.columns)} cell types): {total_marker_genes_sc}')
+    logger.info(f'Remained {sc_dict_name}  in ST: {total_marker_genes_sc - lost_genes}')
+    report_df.loc[sc_dict_name, :] = [total_marker_genes_sc, (total_marker_genes_sc - lost_genes), len(markers_st_df.columns)]
+    st_marker_time3 = time.time()
+    marker_time = np.round(st_marker_time3 - st_marker_time2, 3)
+    logger.info(
+        f"Calculate of ST marker genes for {sc_dict_name} took {marker_time}"
+    )
+
+if not os.path.exists("data/reports"):
+    os.makedirs("data/reports")
+report_df.to_csv("data/reports/" + st_cell_type_path.split("/")[-1], index=False)
 
 end_time = time.time()
 total_time = np.round(end_time - start_time, 3)
 logger.info(
     f"Execution took {total_time}"
 )
-
-report_dict = {"sc_marker_genes": total_marker_genes_sc, "st_marker_genes_25": (total_marker_genes_sc - lost_genes), \
-               "st_cell_types": len(markers_st_df.columns)}
-
-if not os.path.exists("data/reports"):
-    os.makedirs("data/reports")
-pd.DataFrame(report_dict, index=["val"]).to_csv("data/reports/" + st_cell_type_path.split("/")[-1], index=False)
--- scripts/metric.sh
+++ scripts/metric.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Run from ssi directory to create reports in ssi/data/reports
 
-FILES_LB_B5="/goofys/projects/SSI/testing/tangram/L5_B5_265ct_tangram.csv
+FILES_L5_B5="/goofys/projects/SSI/testing/tangram/L5_B5_265ct_tangram.csv
 /goofys/projects/SSI/testing/spatialid/L5_B5_265ct_spatialid.csv
 /goofys/projects/SSI/testing/cytospace/L5_B5_265ct_cytospace.csv"
 
 FILES_10X="/goofys/projects/SSI/testing/cytospace/visium_mouse_brain_cytospace.csv
 /goofys/projects/SSI/testing/seurat/visium_mouse_brain_seurat.csv"
 
-for f in $FILES_LB_B5
+for f in $FILES_L5_B5
 do
 	echo "Processing $f"
     python scripts/metric.py --sc_path /goofys/projects/SSI/datasets/mouse_brain_L5.h5ad --st_path /goofys/Samples/Stereo_seq/Mouse_brain/SS200000141TL_B5.h5ad --st_cell_type_path $f & 
