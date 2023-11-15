561061d5430966351440f3c224bd3569b4e0ed21
Vladimir Kovacevic <upravljac@gmail.com>
2023-11-15 14:07:19 +0000

    Visualize synthetic downsampled data benchmark results.

---
+++ scripts/viz_benchmark.py
@@ -0,0 +1,17 @@
+import glob as glob
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+files = glob.glob('../test/' + 'benchmark_4K_*.csv')
+dfs = {}
+for f in files:
+    name = f.split('_')[-1].replace('.csv', '')
+    dfs[name] = pd.read_csv(f)
+    sns.scatterplot(data=dfs[name], x='Subsample', y='Accuracy')
+plt.legend(labels=dfs.keys())
+plt.xticks(dfs[name].values[:, 0], size=7)
+plt.title('Cell type detection using different distances on Mouse brain 4K dataset')
+plt.savefig('comparison.png', dpi=150)
