9934da0e4a383de12bf5f4367abbba405fa77093
Marija Bezulj <mejasmi@gmail.com>
2023-12-07 13:20:20 +0000

    run_all.sh upadte to CoDi naming

--- run_all.sh
+++ run_all.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # Adult mouse brain stereo-seq 4K (celltype_pred)
-python ssi.py --st_path /goofys/Samples/Stereo_seq/Mouse_brain/adult_mouse_brain_ST4k.h5ad --sc_path /goofys/Samples/sc_reference/adult_mouse_brain_SC4k.h5ad -a cell_subclass
+python CoDi.py --st_path /goofys/Samples/Stereo_seq/Mouse_brain/adult_mouse_brain_ST4k.h5ad --sc_path /goofys/Samples/sc_reference/adult_mouse_brain_SC4k.h5ad -a cell_subclass
 
 # Adult mouse brain stereo-seq 7K
-python ssi.py --st_path /goofys/Samples/Stereo_seq/Mouse_brain/adult_mouse_brain_ST7k.h5ad --sc_path /goofys/Samples/sc_reference/adult_mouse_brain_SC7k.h5ad -a cell_subclass
+python CoDi.py --st_path /goofys/Samples/Stereo_seq/Mouse_brain/adult_mouse_brain_ST7k.h5ad --sc_path /goofys/Samples/sc_reference/adult_mouse_brain_SC7k.h5ad -a cell_subclass
 
 # Adult mouse brain stereo-seq (celltype_pred)
-python ssi.py --st_path /goofys/Samples/Stereo_seq/Mouse_brain/SS200000141TL_B5.h5ad --sc_path /goofys/projects/SSI/datasets/mouse_brain_L5.h5ad  -a cell_subclass --n_jobs 36
+python CoDi.py --st_path /goofys/Samples/Stereo_seq/Mouse_brain/SS200000141TL_B5.h5ad --sc_path /goofys/projects/SSI/datasets/mouse_brain_L5.h5ad  -a cell_subclass --n_jobs 36
 
 # Mouse brain 16.5 stereo-seq (sim anno)
-python ssi.py --st_path /goofys/Samples/Stereo_seq/E16.5_E1S3_cell_bin_whole_brain_noborderct.h5ad --sc_path /goofys/Samples/sc_reference/mouse_brain_L5.h5ad  -a cell_subclass
+python CoDi.py --st_path /goofys/Samples/Stereo_seq/E16.5_E1S3_cell_bin_whole_brain_noborderct.h5ad --sc_path /goofys/Samples/sc_reference/mouse_brain_L5.h5ad  -a cell_subclass
 
 # Mouse brain visium 40K cells
-python ssi.py --st_path /goofys/Samples/10X/mouse_brain_visium_cell2location.h5ad --sc_path /goofys/projects/SSI/datasets/mouse_brain_visium_cell2location/visium_mouse_brain_cell2location_sc.h5ad -a annotation_1
+python CoDi.py --st_path /goofys/Samples/10X/mouse_brain_visium_cell2location.h5ad --sc_path /goofys/projects/SSI/datasets/mouse_brain_visium_cell2location/visium_mouse_brain_cell2location_sc.h5ad -a annotation_1
 
 # Mouse kidney 43636x31053
-python ssi.py --st_path /goofys/Samples/slide_seq/cellxgene_kidney_slide_seq_v2/Puck_191223_19_corr.h5ad --sc_path /goofys/Samples/sc_reference/mouse_kidney/GSE157079_P0_adult.h5ad -a clusters
+python CoDi.py --st_path /goofys/Samples/slide_seq/cellxgene_kidney_slide_seq_v2/Puck_191223_19_corr.h5ad --sc_path /goofys/Samples/sc_reference/mouse_kidney/GSE157079_P0_adult.h5ad -a clusters
 
 # Human heart dataset
-python ssi.py --st_path /goofys/Samples/sc_reference/GSE121893_heart_4933_cells.h5ad --sc_path /goofys/Samples/sc_reference/GSE183852_heart.h5ad -a cell_type --n_jobs 24
+python CoDi.py --st_path /goofys/Samples/sc_reference/GSE121893_heart_4933_cells.h5ad --sc_path /goofys/Samples/sc_reference/GSE183852_heart.h5ad -a cell_type --n_jobs 24
 
 # Mop MERFISH (all 12 slices)
-python ssi.py --st_path /goofys/Samples/MERFISH_dataset/mop/merfish13864.h5ad --sc_path /goofys/Samples/sc_reference/mop_merfish.h5ad -a Allen.cluster_label
+python CoDi.py --st_path /goofys/Samples/MERFISH_dataset/mop/merfish13864.h5ad --sc_path /goofys/Samples/sc_reference/mop_merfish.h5ad -a Allen.cluster_label
 
 # Benchmark synthetic visium
 #python scripts/benchmark.py test/config_40k_visium.json
