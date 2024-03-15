bc694e53efc64e961bb50884fd24f330cb0ed856
Vladimir Kovacevic <upravljac@gmail.com>
2024-03-15 15:29:33 +0000

    Added new factors for subsampled datasets.

--- scripts/create_synthetic.py
+++ scripts/create_synthetic.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import scipy.sparse
 import argparse as ap
-
+import os
 
 def create_synthetic(path_to_anndata, percentages = [0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.9]):
     adata = sc.read_h5ad(path_to_anndata)
@@ -24,8 +24,9 @@ def create_synthetic(path_to_anndata, percentages = [0.05, 0.1, 0.2, 0.3, 0.5, 0
             row[selected_indices] = 0
         adata_copy.X = df_copy.values
         adata_copy.X = scipy.sparse.csr_matrix(adata_copy.X)
-        adata_fname = path.rstrip('.h5ad') + '_' + str(perc) + '.h5ad'
-        adata_copy.write('synthetic_sc_' + str(perc) + '.h5ad')
+        adata_fname = os.path.basename(path).rstrip('.h5ad') + '_' + str(perc) + '.h5ad'
+        adata_copy.write(adata_fname)
+        print(f'Writing {adata_fname}...')
 
 
 
@@ -33,7 +34,7 @@ def create_synthetic(path_to_anndata, percentages = [0.05, 0.1, 0.2, 0.3, 0.5, 0
 if __name__ == '__main__':
     parser = ap.ArgumentParser()
     parser.add_argument('--path', help='Path to anndata object.', type=str, required=True)
-    parser.add_argument('--percentages', help='Comma separated list of percentages', type=str, required=False, default="0.05,0.1,0.2,0.3,0.5,0.7,0.9")
+    parser.add_argument('--percentages', help='Comma separated list of percentages', type=str, required=False, default="0.05,0.1,0.2,0.3,0.5,0.6,0.7,0.8,0.9")
     
     args = parser.parse_args()
     path = args.path
