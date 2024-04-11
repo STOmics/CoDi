5a813fd68b381867e0f879bde87d4e7459562a10
Vladimir Kovacevic <upravljac@gmail.com>
2024-04-11 11:57:48 +0000

    Generate subsampled figure for all distances.

--- scripts/viz_benchmark.py
+++ scripts/viz_benchmark.py
@@ -6,10 +6,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 dataset_prefix = "config_40k_visium"
-dataset_prefix = "config_breast_distances"
-dataset_prefix = "config_40k_visium_distances"
+dataset_prefix = "config_breast"
+# dataset_prefix = "config_breast_distances"
+# dataset_prefix = "config_40k_visium_distances"
+title_dataset_name = 'Visium Mouse brain'
+title_dataset_name = 'snRNA breast'
 dataset_name = dataset_prefix.rsplit('config_')[1]
-compare_CoDi_metrics = True
+compare_CoDi_metrics = False
 output_suffix = ''
 
 cur_path = os.path.dirname(os.path.realpath(__file__))
@@ -34,9 +37,11 @@ for f in sorted(files):
     dfs[name] = pd.read_csv(f)
     print(len(dfs[name]), name)
     sns.scatterplot(data=dfs[name], x='Subsample', y='Accuracy')
-plt.legend(labels=dfs.keys())
+plt.legend(labels=dfs.keys(), fontsize="7", loc ="best")  # center right, upper right
+print('********', list(dfs.keys()))
 plt.xticks(dfs[list(dfs.keys())[0]]['Subsample'].values, size=7)
-plt.title(f'Cell type detection accuracy for {dataset_name}')
+plt.title(f'Cell type detection accuracy for subsampled {title_dataset_name}')
 plt.box(False)
+plt.xlabel('Subsample factor')
 plt.ylim([0.6, 1.05])
-plt.savefig(output_path + '.png', dpi=300)
+plt.savefig(output_path + '.png', dpi=300, bbox_inches='tight')
