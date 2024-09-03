4d1969286179296173d79d27b9bf0933bae0d306
Vladimir Kovacevic <upravljac@gmail.com>
2024-09-03 13:14:57 +0200

    Setup file added for creating pip package.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# Load requirements from requirements.txt
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name="codi_bg",
+    version="0.1.0",
+    description="CoDi - Contrastive Distance reference-based cell type annotation for spatial transcriptomics.",
+    url="https://github.com/stomics/codi",
+    author="Vladimir Kovacevic",
+    author_email="vladimirkovacevic@genomics.cn",
+    license="MIT",
+    package_dir={"": "core"},  # Specifies that packages are under the src directory
+    packages=find_packages(where="src"),  # Finds packages in the src directory
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'CoDi=CoDi:main',  # Points to the main function in CoDi.py
+        ],
+    },
+    python_requires='>=3.8',
+)
+
